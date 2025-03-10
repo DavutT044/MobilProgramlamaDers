508cda730248bdbe19cecb0fd240c217ed36f31c
DavutT044 <dvt.trn34@gmail.com>
2025-03-10 12:11:04 +0300

    plakaUyg
    commit2

---
+++ PlakaSehirEslestirme/app/src/main/java/com/example/plakasehireslestirme/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.plakasehireslestirme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Random rnd = new Random();
+        Button btn1;
+        String[] sehirler = {
+                "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin",
+                "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur",
+                "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan",
+                "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İstanbul",
+                "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir",
+                "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş",
+                "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop",
+                "Sivas", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak"
+        };
+        ListView listPlaka = findViewById(R.id.lwPlaka);
+        ListView listSehir = findViewById(R.id.lwSehir);
+        btn1 = findViewById(R.id.button);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Integer> plakaListesi = new ArrayList<>();
+                List<String> sehirListesi = new ArrayList<>();
+
+                for (int i = 0; i < 10; i++) {
+                    int random1 = rnd.nextInt(81);
+                    plakaListesi.add(random1 + 1);
+                    sehirListesi.add(sehirler[random1]);
+                }
+                ArrayAdapter<Integer> plakaAdapter = new ArrayAdapter<>
+                        (MainActivity.this, android.R.layout.simple_list_item_1, plakaListesi);
+                ArrayAdapter<String> sehirAdapter = new ArrayAdapter<>
+                        (MainActivity.this, android.R.layout.simple_list_item_1, sehirListesi);
+                listPlaka.setAdapter(plakaAdapter);
+                listSehir.setAdapter(sehirAdapter);
+            }
+        });
+
+        listSehir.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
